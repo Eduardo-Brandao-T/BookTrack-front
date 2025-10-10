249a80fdf3306f23d11eb1342e217a40b14cf048
Eduardo Brandao <eduardo.brandao.t1986@gmail.com>
2025-10-10 13:16:25 -0300

    vite and tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
+      Tailwind funcionando! 🚀
+    </div>
+  )
+}
+
+export default App
